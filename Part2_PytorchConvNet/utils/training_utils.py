Fixed figures for Part2 to show average loss per epoch of training data.

Part2_PytorchConvNet/utils/training_utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-def train_model(model, optimizer, loader_train, loader_val, num_epochs, print_every, device):
+def train_model(model, optimizer, loader_train, loader_val, num_epochs, device):
     """
     Trains model on MNIST using optimizer object.
     
@@ -18,10 +18,10 @@ def train_model(model, optimizer, loader_train, loader_val, num_epochs, print_ev
     train_history = {'train_loss_hist':[], 'val_loss_hist':[],
                      'train_acc_hist':[], 'val_acc_hist':[]}
     
-    iteration_count = 0
-    it = 0
     for epoch in range(num_epochs):
-        iteration_count += it
+        total_train_size = 0
+        total_train_loss = 0
+        total_train_correct = 0
         for it, (X, y) in enumerate(loader_train):
             # Move inputs to specified device
             X = X.to(device=device, dtype=torch.float32)
@@ -36,23 +36,28 @@ def train_model(model, optimizer, loader_train, loader_val, num_epochs, print_ev
             optimizer.zero_grad() # Zero out the gradients
             train_loss.backward() # Compute gradient of loss w.r.t. model parameters
             optimizer.step() # Make a gradient update step
+            minibatch_size = y.size(0)
 
-            if (it+iteration_count) % print_every == 0:
-                ### Update train_history
-                # train_loss_hist & train_acc_hist
-                _, preds = scores.max(dim=1)
-                train_acc = 100*(float((scores.max(dim=1)[1] == y).sum()) / y.size(0))
-                train_history['train_loss_hist'].append(train_loss.item())
-                train_history['train_acc_hist'].append(train_acc)
-                # val_loss_hist & val_acc_hist
-                val_acc, val_loss = evaluation(model, loader_val, criterion, device=device)
-                train_history['val_loss_hist'].append(val_loss.item())
-                train_history['val_acc_hist'].append(val_acc)
+            # Epoch loss and accuracy average
+            total_train_size += minibatch_size
+            total_train_loss += (minibatch_size * train_loss).item()
+            total_train_correct += (scores.max(dim=1)[1] == y).sum()
 
-                # Print training process
-                print('Epoch %d, Iteration %d:' % (epoch+1, it+iteration_count))
-                print('Training data: loss = %.4f, accuracy = %.2f' % (train_loss.item(), train_acc))
-                print('Validation data: loss = %.4f, accuracy = %.2f\n' % (val_loss.item(), val_acc))
+        ### Update train_history
+        # train_loss_hist & train_acc_hist
+        train_epoch_loss = total_train_loss / total_train_size
+        train_epoch_acc = 100 * (float(total_train_correct) / total_train_size)
+        train_history['train_loss_hist'].append(train_epoch_loss)
+        train_history['train_acc_hist'].append(train_epoch_acc)
+        # val_loss_hist & val_acc_hist
+        val_acc, val_loss = evaluation(model, loader_val, criterion, device=device)
+        train_history['val_loss_hist'].append(val_loss.item())
+        train_history['val_acc_hist'].append(val_acc)
+
+        # Print training process
+        print('Epoch %d:' % (epoch+1))
+        print('Training data: loss = %.4f, accuracy = %.2f' % (train_epoch_loss, train_epoch_acc))
+        print('Validation data: loss = %.4f, accuracy = %.2f' % (val_loss.item(), val_acc))
         print('-----------')
     return train_history
 